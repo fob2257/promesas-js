63ccc5c54345138ecef65e9eca942f1a336d5fa5
Saul Meneses Alaniz <saul.meneses@rosolutionsdev.com.mx>
2019-02-01 17:11:31 -0700

    Simple promise

--- promise.js
+++ promise.js
@@ -1,3 +1,5 @@
-const { goodPromise, badPromise } = require('./promises');
+const goodPromise = new Promise((resolve, reject) => {
+    resolve('promise resolved!');
+});
 
-goodPromise().then(result => console.log(result));
+goodPromise.then(result => console.log(result));
